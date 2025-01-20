84d8205d36b5c0c89a20096964b15192b9348689
Abdullah-Albanna <albannaa78@gmail.com>
2025-01-20 23:30:59 +0300

    removed the other macro

--- src/parsers.rs
+++ src/parsers.rs
@@ -15,7 +15,21 @@ use crate::{
     models::{JsonSchema, JsonSchemaTypes},
 };
 
-use schema2struct_macros::update_schema_fields;
+macro_rules! update_schema_fields {
+    ($schema:expr, $value:expr, $key_span:expr, $value_span:expr, [
+        $($field:ident),* $(,)?
+    ]) => {
+        $(
+            if $schema.$field.is_none() && $value.$field.is_some() {
+                $schema.$field = $value.$field;
+
+                paste::paste! {
+                    $schema.[<$field _span>] = Some(($key_span, $value_span));
+                }
+            }
+        )*
+    };
+}
 
 /// Tracks the current depth of schema parsing
 ///
